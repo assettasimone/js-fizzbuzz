4f2c4657471e2c0b12bcd8c87e9795bafde3b730
assettasimone <assettasimone@gmail.com>
2025-07-10 18:08:37 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+/* Scrivi un programma che stampi i numeri da 1 a 100,
+ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz. */
+
+/* facciamo un cliclo for per scorrere e stampare i numeri da 1 a 100 */
+for (let i = 1; i <= 100; i++) {
+
+    /* controlliamo tramite un if se il numero è divisibile per 3 */
+    if (i % 3) {
+
+    } else if ()
+
+}
+
